2373a856e4d957b57b2d2c8b3d50f21df5a77d35
Elbachir <elbachirzriguinat@gmail.com>
2026-01-21 22:40:55 +0100

    routing path and config

--- rick-morty/src/app/app.routes.ts
+++ rick-morty/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { CharacterComponent } from './character-component/character-component';
+import { CharacterComponentDetaill } from './character-component.detaill/character-component.detaill';
+import { Capitulo } from './capitulo/capitulo';
+import { CapituloDetaill } from './capitulo-detaill/capitulo-detaill';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'character',component:character_comp},
+    {path: 'character/:id',component:character_comp_detaill},
+    {path: 'capitulo',component:capitulo_component},
+    {path: 'character/:id',component:capitulo_component_detaill},
+    {path: '',component:PageNotFound},
+];
